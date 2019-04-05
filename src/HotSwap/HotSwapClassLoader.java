dc0139ab3c0759f4c40cb463f8e0fb2eaf9a9a4b
Mason_z <375359554@qq.com>
2019-04-05 17:44:57 +0800

    HotSwap

---
+++ src/HotSwap/HotSwapClassLoader.java
@@ -0,0 +1,14 @@
+package HotSwap;
+
+/**
+ * @Author zz
+ * @Date
+ **/
+public class HotSwapClassLoader extends ClassLoader{
+    public HotSwapClassLoader(){
+        super(HotSwapClassLoader.class.getClassLoader());
+    }
+    public Class loadByte(byte[] classByte){
+        return defineClass(null,classByte,0,classByte.length);
+    }
+}
